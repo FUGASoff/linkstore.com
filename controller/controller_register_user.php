765437b911d4b5dfaa6118d7ebfb8cb22f2a0453
FUGASoff <Dr-FUGASoff@yandex.ru>
2015-10-08 16:57:56 +0600

    First try of creation MVC Only Core and Model

---
+++ controller/controller_register_user.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrey
+ * Date: 08.10.15
+ * Time: 15:32
+ */
+class controller_register_user extends controller
+{
+   /* function __construct()
+    {
+        /*$this->model = new model_user();
+        $this->view = new View();
+    }
+
+    function action_index()
+    {
+        $data = $this->model->get_data();
+        $this->view->generate('register_view.php', 'template_view.php', $data);*/
+
+}
