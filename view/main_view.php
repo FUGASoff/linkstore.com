dcaa1da39454120745ee159e1d4c6b4991612a7d
FUGASoff <Dr-FUGASoff@yandex.ru>
2015-10-26 16:27:05 +0600

    main_view

---
+++ view/main_view.php
@@ -0,0 +1,31 @@
+<?php require 'header.php';
+$view_set=$this->view_set;
+if ($view_set['msg_code']!=0)
+{
+    echo '<div class="page-header">';
+    $msg=$view_set['msg'];
+    if ($view_set['msg_code']==1)
+    {
+    echo'<h1>Seccess!=)</h1>
+        <div class="alert alert-success">';
+        echo $msg;
+        echo'</div></div>';
+    }
+    if ($view_set['msg_code']==2)
+    {
+        echo'<h1>Atention!=|</h1>
+        <div class="alert alert-info">';
+        echo $msg;
+        echo'</div></div>';
+    }
+    if ($view_set['msg_code']==3)
+    {
+        echo'<h1>We have a problem!=(</h1><div class="alert alert-danger">';
+        echo $msg;
+        echo'</div></div>';
+    }
+    $view_set['msg_code']=0;
+}
+$file='view/' . $view_set['body_name'] . '.php';
+require_once $file;
+require 'footer.php'; ?>
